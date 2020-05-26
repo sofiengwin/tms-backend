22b2582212af1e3f80a6e3bf88888812b950b849
Ogbara Godwin <godwin.onisofien@andela.com>
2020-05-26 09:42:56 +0100

    dotenv is only used in development

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,9 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+if Rails.env.development?
+  Dotenv::Railtie.load
+end
 
 module Backend
   class Application < Rails::Application
